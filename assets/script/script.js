b1c33e911f3dde86bd9b23011399cb957b6f65f1
Dmitriy <ubersweet@yandex.com>
2023-01-08 14:10:33 +0300

    Release Section Gallery (v.1)

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,9 @@ const cards = document.querySelectorAll('.card');
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
+    if (card.classList.contains('active')) {
+      return card.classList.remove('active');
+    }
     clearActiveClasses();
     card.classList.add('active');
   });
